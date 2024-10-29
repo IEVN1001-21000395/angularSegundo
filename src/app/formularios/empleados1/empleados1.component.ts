89ec1ae60594c5894e48172e00a4df404b2e1659
Cristian Santiago Serrano Ferro <cserranoferro171@gmail.com>
2024-10-28 19:42:13 -0600

    Empleados final

--- src/app/formularios/empleados1/empleados1.component.ts
+++ src/app/formularios/empleados1/empleados1.component.ts
@@ -19,7 +19,8 @@ interface Usuario {
 })
 export default class Empleados1Component {
   formGroup!: FormGroup;
-  personas: Usuario[] = [];
+  personas: any[] = [];
+  totalGeneral: number = 0;
   totalPagarh: number = 0;
   totalHorasExtra: number = 0;
 
@@ -90,6 +91,7 @@ export default class Empleados1Component {
       this.personas = JSON.parse(personasGuardadas);
     }
   }
+
   buscarPorMatricula(): void {
     const matriculaBuscada = this.formGroup.value.matricula;
     
@@ -110,36 +112,46 @@ export default class Empleados1Component {
       });
     } 
   }
-  
+  eliminarPorMatricula(): void {
+  const matriculaBuscada = this.formGroup.value.matricula;
 
-  Horasxpagar(): void {
-    const personasGuardadas = localStorage.getItem('personas');
+  const personasGuardadas = localStorage.getItem('personas');
+  let personas = personasGuardadas ? JSON.parse(personasGuardadas) : [];
+  personas = personas.filter((persona: Usuario) => persona.matricula !== matriculaBuscada);
+  localStorage.setItem('personas', JSON.stringify(personas));
+  this.formGroup.reset();
 
-    if (personasGuardadas) {
-        const personas = JSON.parse(personasGuardadas);
-        const costoPorHora = 70; 
-        const costoHoraExtra = 140; 
-        let totalPagarh = 0;
-        let totalHorasExtra = 0;
-
-        for (const persona of personas) {
-            const horasTrabajadas = persona.horas || 0; 
-            let horasNormales = 0;
-            let horasExtras = 0;
-
-            if (horasTrabajadas <= 40) {
-                horasNormales = 40; 
-                horasExtras = horasTrabajadas - 40;
-            } else {
-                horasNormales = horasTrabajadas; 
-            }
-            totalPagarh = horasNormales * costoPorHora;
-            totalHorasExtra = horasExtras * costoHoraExtra;
-        }
-        this.totalPagarh = totalPagarh; 
-        this.totalHorasExtra = totalHorasExtra; 
-    } 
+  alert('Persona eliminada con éxito.');
 }
 
+Horasxpagar(): void {
+  const personasGuardadas = localStorage.getItem('personas');
+
+  if (personasGuardadas) {
+      const personas = JSON.parse(personasGuardadas);
+      const costoPorHora = 70; 
+      const costoHoraExtra = 140; 
+
+      this.personas = personas.map((persona: any) => {
+          const horasTrabajadas = persona.horas || 0;
+          let horasNormales = Math.min(horasTrabajadas, 40);
+          let horasExtras = horasTrabajadas > 40 ? horasTrabajadas - 40 : 0;
+
+          const totalPagarh = horasNormales * costoPorHora;
+          const totalHorasExtra = horasExtras * costoHoraExtra;
+          const subTotal = totalPagarh + totalHorasExtra;
+
+          return {
+              ...persona,
+              horasNormales,
+              horasExtras,
+              totalPagarh,
+              totalHorasExtra,
+              subTotal
+          };
+      });
+      this.totalGeneral = this.personas.reduce((acc, persona) => acc + persona.subTotal, 0);
+  }
+}
     
 }
