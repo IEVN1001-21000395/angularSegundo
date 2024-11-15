e6fcf8cbcfbab51764c13d914f92103862155a9f
Cristian Santiago Serrano Ferro <cserranoferro171@gmail.com>
2024-11-14 21:00:36 -0600

    Alumnos Final

--- src/app/utl/alumnos/alumnos.component.ts
+++ src/app/utl/alumnos/alumnos.component.ts
@@ -2,9 +2,10 @@ import { Component,  OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AlumnoFilterPipe } from '../alumnos-filter.pipe';
 import { CommonModule } from '@angular/common';
-import { AlumnosUtl } from '../interfaces/alumnosutl';
+//import { AlumnosUtl } from '../interfaces/alumnos';
 import { ProyectoapiService } from '../proyectoapi.service';
 import { RouterLink } from '@angular/router';
+import { AlumnosUtl } from '../interfaces/alumnosutl';
  
  
  
